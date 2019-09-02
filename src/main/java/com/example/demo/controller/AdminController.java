616e9e8b954d994b04178566523e2f9bb3f1c416
zsxa1323 <ehdgusdl1323@gmail.com>
2019-09-02 18:33:26 +0900

    메인화면 연결

---
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Repository
+public class AdminController {
+
+	@RequestMapping
+	public String main() {
+		
+		
+		return "main";
+	}
+	
+	
+}
+
