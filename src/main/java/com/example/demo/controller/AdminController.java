11d4789928ee1459e1701c8dc8ff654fa5fcb4d9
zsxa1323 <ehdgusdl1323@gmail.com>
2019-09-03 10:44:23 +0900

    게시판 화면 추가

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Repository
+@Controller
 public class AdminController {
 
-	@RequestMapping
+	@RequestMapping("/main")
 	public String main() {
 		
 		
